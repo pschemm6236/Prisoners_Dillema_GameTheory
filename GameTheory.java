b9b0ed636c25509851f6d5767f16a2ca8f6b9278
pschemm6236 <75461114+pschemm6236@users.noreply.github.com>
2026-04-08 14:19:51 -0400

    Modify game round logic and update beginRound method

--- GameTheory.java
+++ GameTheory.java
@@ -45,9 +45,25 @@ public class GameTheory {
         System.out.println("Number of rounds for this game is " + getRound()); 
 
         int roundCounter = 1;
-        while(roundCounter < round){
+        while(roundCounter <= round){
             System.out.println("Round #: " + roundCounter);
-            beginRound(p1, p2);
+            beginRound(p1, p2, roundCounter);
+            roundCounter++;
+        }
+        
+        //determine winner
+        System.out.println("\nFinal Scores:");
+        System.out.println(p1.getName() + ": " + p1.getCoinTotal() + " coins");
+        System.out.println(p2.getName() + ": " + p2.getCoinTotal() + " coins");
+        
+        if (p1.getCoinTotal() > p2.getCoinTotal()) {
+            setGameWinner(p1);
+            System.out.println(p1.getName() + " wins!");
+        } else if (p2.getCoinTotal() > p1.getCoinTotal()) {
+            setGameWinner(p2);
+            System.out.println(p2.getName() + " wins!");
+        } else {
+            System.out.println("It's a tie!");
         }
         
 
@@ -58,9 +74,43 @@ public class GameTheory {
      * Contains logic for different strategies in game
      * ex: tit for tat going against always defect 
      */
-    public Player beginRound(Strategy p1, Strategy p2) {
+    public Player beginRound(Strategy p1, Strategy p2, int roundNumber) {
 
         System.out.println("");
+        
+        boolean p1Move = p1.decideMove(roundNumber, p2);
+        boolean p2Move = p2.decideMove(roundNumber, p1);
+        
+        //store decisions
+        p1.getRoundDecision()[roundNumber - 1] = p1Move;
+        p2.getRoundDecision()[roundNumber - 1] = p2Move;
+        
+        //calculate points
+        int p1Points = 0;
+        int p2Points = 0;
+        
+        if (p1Move && p2Move) { // both cooperate
+            p1Points = 3;
+            p2Points = 3;
+        } else if (p1Move && !p2Move) { // p1 coop, p2 def
+            p1Points = 0;
+            p2Points = 5;
+        } else if (!p1Move && p2Move) { // p1 def, p2 coop
+            p1Points = 5;
+            p2Points = 0;
+        } else { // both def
+            p1Points = 1;
+            p2Points = 1;
+        }
+        
+        //update totals
+        p1.setCoinTotal(p1.getCoinTotal() + p1Points);
+        p2.setCoinTotal(p2.getCoinTotal() + p2Points);
+        p1.setTotalMovesMade(p1.getTotalMovesMade() + 1);
+        p2.setTotalMovesMade(p2.getTotalMovesMade() + 1);
+        
+        System.out.println(p1.getName() + " chose " + (p1Move ? "cooperate" : "defect") + ", " + p2.getName() + " chose " + (p2Move ? "cooperate" : "defect"));
+        System.out.println("Points: " + p1.getName() + " +" + p1Points + ", " + p2.getName() + " +" + p2Points);
         
         return null; //return the winner of the round 
     }
